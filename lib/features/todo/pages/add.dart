9de837bfeb852c92ff8a1bbb1ef0886a50cac3e3
MurtazaFlutter <ghulammurtazagopang3@gmail.com>
2023-11-20 09:10:24 +0500

    some progress

--- lib/features/todo/pages/add.dart
+++ lib/features/todo/pages/add.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:task_management/common/utils/app_imports.dart';
+import 'package:task_management/common/widgets/custom_outline_button.dart';
 import 'package:task_management/common/widgets/custom_text_field.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
+    as picker;
+import 'package:task_management/features/todo/controllers/dates/date_provider.dart';
 
 class AddTask extends ConsumerStatefulWidget {
   const AddTask({super.key});
@@ -15,6 +18,7 @@ class _AddTaskState extends ConsumerState<AddTask> {
   late final TextEditingController _des = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    var scheduleDate = ref.watch(dateStateProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -39,6 +43,63 @@ class _AddTaskState extends ConsumerState<AddTask> {
               controller: _des,
               hintStyle:
                   appStyle(16.sp, AppConstants.kGreyLight, FontWeight.w500),
+            ),
+            const Gap(20),
+            CustomOutLineButton(
+              onTap: () {
+                picker.DatePicker.showDatePicker(context,
+                    showTitleActions: true,
+                    minTime: DateTime(2023, 19, 11),
+                    maxTime: DateTime(2024, 19, 11),
+                    theme: picker.DatePickerTheme(
+                        doneStyle: TextStyle(
+                            color: AppConstants.kGreen,
+                            fontSize: 16.sp)), onConfirm: (date) {
+                  ref.read(dateStateProvider.notifier).setDate(
+                        date.toString(),
+                      );
+                }, currentTime: DateTime.now(), locale: picker.LocaleType.en);
+              },
+              width: AppConstants.kWidth,
+              height: 52.h,
+              color: AppConstants.kBlueLight,
+              text: scheduleDate == ""
+                  ? "Set Date"
+                  : scheduleDate.substring(0, 10),
+            ),
+            const Gap(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomOutLineButton(
+                  onTap: () {
+                    picker.DatePicker.showDatePicker(
+                      context,
+                      showTitleActions: true,
+                      onConfirm: (time) {},
+                    );
+                  },
+                  width: AppConstants.kWidth * 0.4,
+                  height: 52.h,
+                  color: AppConstants.kBlueLight,
+                  text: "Start time",
+                ),
+                CustomOutLineButton(
+                  onTap: () {},
+                  width: AppConstants.kWidth * 0.4,
+                  height: 52.h,
+                  color: AppConstants.kBlueLight,
+                  text: "End time",
+                ),
+              ],
+            ),
+            const Gap(20),
+            CustomOutLineButton(
+              onTap: () {},
+              width: AppConstants.kWidth,
+              height: 52.h,
+              color: AppConstants.kBlueLight,
+              text: "Submit",
             ),
           ],
         ),
